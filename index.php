8dae28cb75e5c78894a228d566b6af4f13776d16
lihai <seven.lihai@gmail.com>
2014-02-22 17:18:55 +0800

    test

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 define('PHPCMS_PATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 define('APPLICATION_PATH', "/home/herry/data/runtime/application.log");
 include PHPCMS_PATH.'/phpcms/base.php';
-file_put_contents(APPLICATION_PATH,"进入系统",FILE_APPEND);
 pc_base::creat_app();
 ?>
 
